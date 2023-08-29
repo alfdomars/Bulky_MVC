40d2a42387c17901f45575c22fb54060b7cbb9fb
alfdomars <alfdonyusmarschaal@gmail.com>
2023-08-30 06:48:48 +0700

    30/08/2023

--- BUlky.DataAccess/Repository/ProductRepository.cs
+++ BUlky.DataAccess/Repository/ProductRepository.cs
@@ -14,11 +14,27 @@ namespace Bulky.DataAccess.Repository
         private ApplicationDbContext _db;
         public ProductRepository(ApplicationDbContext db) : base(db)
         {
-            _db=db;
+            _db = db;
         }
         public void Update(Product obj)
         {
-            _db.Products.Update(obj);
+            var objFromDb = _db.Products.FirstOrDefault(u => u.Id == obj.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.Title = objFromDb.Title;
+                objFromDb.Description = objFromDb.Description;
+                objFromDb.ISBN = objFromDb.ISBN;
+                objFromDb.Author = objFromDb.Author;
+                objFromDb.ListPrice = objFromDb.ListPrice;
+                objFromDb.Price = objFromDb.Price;
+                objFromDb.Price50 = objFromDb.Price50;
+                objFromDb.Price100 = objFromDb.Price100;
+                objFromDb.CategoryId = objFromDb.CategoryId;
+                if (obj.ImageUrl != null)
+                {
+                    objFromDb.ImageUrl = obj.ImageUrl;
+                }
+            }
         }
     }
 }
---
+++ BulkyWeb/wwwroot/js/product.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function () {
+    loadDataTable();
+});
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url: '/admin/product/getall' },
+        "columns": [
+            { data: 'title' },
+            { data: 'isbn' },
+            { data: 'listPrice' },
+            { data: 'author' },
+            { data: 'category.name' }
+        ]
+    });
+}
